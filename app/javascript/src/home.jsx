ad36044ac01bc78f8c54f9c47652e8632ddcef78
h8rry <harrychenca@gmail.com>
2024-01-25 16:24:58 +0800

    Fixes for creating a user in home.jsx

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -1,40 +1,67 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import Layout from './layout';
 import './home.scss';
+import { safeCredentials, handleErrors } from './utils/fetchHelper';
 
-const Home = () => (
-  <Layout>
-    <div class="row">
-      <div class="col-5 mt-5 offset-2" id="hero">
-        <h3 id="welcome">Welcome to Twitter.</h3>
-        <p>Connect with your friends - and other fascinating people. Get in-the-moment update on the things that interest you. And watch events unfold, in real time, from every angle.</p>
-      </div>
-      <div class="col-4 mt-5">
-        <div class="row rounded" id="acc">
-          <form id="log-in">
-            <h6 class="mb-3">Welcome Back!</h6>
-            <input type="text"  class="inputwidth username" id="usernameinput" placeholder="Username" required></input>
-            <input type="text" class="password" id="passinput" placeholder="Password" required></input>
-            <button id="log-in-btn">Sign In</button>
-            <label for="remember"><input type="checkbox" id="remember" class="me-1"></input>
-            Remember me</label>
-            <a href="#" class="ms-3">Forgot Password?</a>
-          </form>
+const Home = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    fetch('/api/users', safeCredentials({
+      method: 'POST',
+      body: JSON.stringify({
+        user: {
+          username: username,
+          email: email,
+          password: password,
+        }
+      })
+    }))
+      .then(handleErrors)
+      .then(data => {
+        console.log(data);
+        // window.location = '/';
+      })
+  };
+
+  return (
+    <Layout>
+      <div className="row">
+        <div className="col-5 mt-5 offset-2" id="hero">
+          <h3 id="welcome">Welcome to Twitter.</h3>
+          <p>Connect with your friends - and other fascinating people. Get in-the-moment update on the things that interest you. And watch events unfold, in real time, from every angle.</p>
         </div>
-        <div class="row rounded mt-3" id="acc">
-          <form class="sign-up">
-            <h6 class="mb-3">New to Twitter?</h6>
-            <input type="text" class="inputwidth email" id="newemail" placeholder="Email" required></input>
-            <input type="text" class="inputwidth username" id="newusername" placeholder="Username" required></input>
-            <input type="text" class="inputwidth password" id="newpass" placeholder="Password" required></input>
-            <button class="inputwidth" id="sign-up-btn">Sign Up for Twitter</button>
-          </form>
+        <div className="col-4 mt-5">
+          <div className="row rounded" id="acc">
+            <form id="log-in">
+              <h6 className="mb-3">Welcome Back!</h6>
+              <input type="text"  className="inputwidth username" id="usernameinput" placeholder="Username" required></input>
+              <input type="text" className="password" id="passinput" placeholder="Password" required></input>
+              <button id="log-in-btn">Sign In</button>
+              <label htmlFor="remember"><input type="checkbox" id="remember" className="me-1"></input>
+              Remember me</label>
+              <a href="#" className="ms-3">Forgot Password?</a>
+            </form>
+          </div>
+          <div className="row rounded mt-3" id="acc">
+            <form className="sign-up" onSubmit={handleFormSubmit}>
+              <h6 className="mb-3">New to Twitter?</h6>
+              <input type="text" className="inputwidth email" id="newemail" placeholder="Email" required onChange={(event) => setEmail(event.target.value)}></input>
+              <input type="text" className="inputwidth username" id="newusername" placeholder="Username" required onChange={(event) => setUsername(event.target.value)}></input>
+              <input type="text" className="inputwidth password" id="newpass" placeholder="Password" required onChange={(event) => setPassword(event.target.value)}></input>
+              <button className="inputwidth" id="sign-up-btn">Sign Up for Twitter</button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  </Layout>
-)
+    </Layout>
+  );
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
